168878519665b89bfc5d0a88dd5448e23a80a40d
Tom Ingold <tingold@Admins-MacBook-Pro.local>
2019-02-12 17:07:10 -0500

    added test for mosaic -- wip

---
+++ macro/mosaic_test.go
@@ -0,0 +1,29 @@
+package macro
+
+import (
+	"github.com/boundlessgeo/go-gdal"
+	"testing"
+)
+
+
+const S3_BUCKET_URL = "/vsis3/imagerytest"
+const S3_FILE  = "/vsis3/imagerytest/m_3707501_ne_18_1_20150801.tif"
+
+func TestMosaic(t *testing.T) {
+	type args struct {
+		urls []string
+		epsg string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{ name: "everything", args: args{urls: gdal.VSIReadDirRecursiveAbs(S3_BUCKET_URL), epsg: "EPSG:4326"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Mosaic(tt.args.urls, tt.args.epsg)
+		})
+	}
+}
